ff8dcd7c6706e8631d7b014a8634f0e3b66c2321
rokhayaaa <82120372+rokhayaaa@users.noreply.github.com>
2021-05-04 23:35:23 +0000

    Update DataLoader.sql

--- Covid-19_Progression_Modeler/DataLoader/DataLoader.sql
+++ Covid-19_Progression_Modeler/DataLoader/DataLoader.sql
@@ -1,5 +1,5 @@
 --Création de la base de donnée DataLoader
-CREATE DATABASE IF NOT EXISTS `DataLoader` ;
+CREATE DATABASE IF NOT EXISTS `DataLoader`;
 
 --Utilisation de la base de donnée DataLoader
 USE `DataLoader`;
@@ -16,26 +16,27 @@ CREATE TABLE IF NOT EXISTS `Communiques` (
   `nb_deces` int(5) NOT NULL DEFAULT 0,
   `date_extraction`varchar(10)  DEFAULT NULL
 
-) ;
+);
 
 --Création de la table Regions
 CREATE TABLE IF NOT EXISTS `Regions` (
   `NomRegion` varchar(50) DEFAULT "NULL" PRIMARY KEY,
   `Nbre_Cas` int(5) NOT NULL DEFAULT 0
   
-) ;
-INSERT INTO `Regions` VALUES ("DAKAR",0) ;
-INSERT INTO `Regions` VALUES ("DIOURBEL",0) ;
-INSERT INTO `Regions` VALUES ("FATICK",0) ;
-INSERT INTO `Regions` VALUES ("KEDOUGOU",0) ;
-INSERT INTO `Regions` VALUES ("KAFFRINE",0) ;
-INSERT INTO `Regions` VALUES ("KAOLACK",0) ;
-INSERT INTO `Regions` VALUES ("KOLDA",0) ;
-INSERT INTO `Regions` VALUES ("LOUGA",0) ;
-INSERT INTO `Regions` VALUES ("MATAM",0) ;
-INSERT INTO `Regions` VALUES ("SEDHIOU",0) ;
-INSERT INTO `Regions` VALUES ("SAINT-LOUIS",0) ;
-INSERT INTO `Regions` VALUES ("TAMBACOUNDA",0) ;
-INSERT INTO `Regions` VALUES ("THIES",0) ;
-INSERT INTO `Regions` VALUES ("ZIGUINCHOR",0) ;
+);
 
+--Insertion de valeur dans la table Region
+INSERT INTO `Regions` VALUES ("DAKAR",0);
+INSERT INTO `Regions` VALUES ("DIOURBEL",0);
+INSERT INTO `Regions` VALUES ("FATICK",0);
+INSERT INTO `Regions` VALUES ("KEDOUGOU",0);
+INSERT INTO `Regions` VALUES ("KAFFRINE",0);
+INSERT INTO `Regions` VALUES ("KAOLACK",0);
+INSERT INTO `Regions` VALUES ("KOLDA",0);
+INSERT INTO `Regions` VALUES ("LOUGA",0);
+INSERT INTO `Regions` VALUES ("MATAM",0);
+INSERT INTO `Regions` VALUES ("SEDHIOU",0);
+INSERT INTO `Regions` VALUES ("SAINT-LOUIS",0);
+INSERT INTO `Regions` VALUES ("TAMBACOUNDA",0);
+INSERT INTO `Regions` VALUES ("THIES",0);
+INSERT INTO `Regions` VALUES ("ZIGUINCHOR",0);
