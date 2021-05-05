04019f4ddfcbaf3f7701e7d214c53115f738cbe4
cab-0807 <cheikhahmadoubambaba@esp.sn>
2021-05-05 00:53:34 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+brew install python@3.8
+brew install python3-pip 
+
+pip3 install pandas
+pip3 install numpy
+pip3 install tweepy
+pip3 install requests
+pip3 install tqdm
+pip3 install bs4
+pip3 install requests-oauthlib
+pip3 install datetime
+pip3 install PyQt5
+pip3 install pytesseract
+pip3 install simplejson
+pip3 install pdf2image
+pip3 install regex
+pip3 install matplotlib
+pip3 install pylibjpeg-openjpeg
+brew install python-poppler
+brew install poppler-utils
+brew install python3-tk
+pip3 install mysql-connector-python
+pip3 install pytk
+brew install libaio1 libaio-dev 
+brew apt-get install mysql-server
+
+python3 Covid-19_Progression_Modeler/main.py 
